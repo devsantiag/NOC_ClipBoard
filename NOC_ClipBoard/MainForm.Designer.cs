fc42c3144f79d87d4c14ae2bd82edaf39f94007e
Fabiano <contatofasantiago@hotmail.com>
2024-09-16 15:35:41 -0300

    NOC_ClipBoard text

---
+++ NOC_ClipBoard/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fjstavares
+ * Date: 16/09/2024
+ * Time: 15:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace NOC_ClipBoard
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.RichTextBox textBox_Content;
+		private System.Windows.Forms.Button btnCopiar;
+		private System.Windows.Forms.Button btnApagar;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textBox_Content = new System.Windows.Forms.RichTextBox();
+			this.btnCopiar = new System.Windows.Forms.Button();
+			this.btnApagar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox_Content
+			// 
+			this.textBox_Content.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox_Content.Location = new System.Drawing.Point(12, 12);
+			this.textBox_Content.Name = "textBox_Content";
+			this.textBox_Content.Size = new System.Drawing.Size(402, 234);
+			this.textBox_Content.TabIndex = 0;
+			this.textBox_Content.Text = "";
+			// 
+			// btnCopiar
+			// 
+			this.btnCopiar.Location = new System.Drawing.Point(285, 252);
+			this.btnCopiar.Name = "btnCopiar";
+			this.btnCopiar.Size = new System.Drawing.Size(129, 53);
+			this.btnCopiar.TabIndex = 1;
+			this.btnCopiar.Text = "Copiar";
+			this.btnCopiar.UseVisualStyleBackColor = true;
+			this.btnCopiar.Click += new System.EventHandler(this.BtnCopiarClick);
+			// 
+			// btnApagar
+			// 
+			this.btnApagar.Location = new System.Drawing.Point(150, 252);
+			this.btnApagar.Name = "btnApagar";
+			this.btnApagar.Size = new System.Drawing.Size(129, 53);
+			this.btnApagar.TabIndex = 2;
+			this.btnApagar.Text = "Apagar";
+			this.btnApagar.UseVisualStyleBackColor = true;
+			this.btnApagar.Click += new System.EventHandler(this.BtnApagarClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(426, 314);
+			this.Controls.Add(this.btnApagar);
+			this.Controls.Add(this.btnCopiar);
+			this.Controls.Add(this.textBox_Content);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "NOC ClipBoard";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ NOC_ClipBoard/MainForm.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * User: fjstavares
+ * Date: 16/09/2024
+ * Time: 15:17
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NOC_ClipBoard
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+		void BtnCopiarClick(object sender, EventArgs e)
+		{
+			Clipboard.SetText(textBox_Content.Text);
+			textBox_Content.Clear();
+		}
+		void BtnApagarClick(object sender, EventArgs e)
+		{
+			textBox_Content.Clear();
+		}
+	}
+}
